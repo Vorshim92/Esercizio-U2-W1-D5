2b8fe7f094fc2b004b1f73f16ba4855aa2a5aeb7
Vorshim92 <vorshim92@gmail.com>
2024-02-18 12:14:38 +0100

    fix qua e la

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 window.onload = () => {
-  //funzione per transizione header bg-color
-  var header = document.querySelector("header");
-  var divSottostante = document.querySelector(".div-trending");
-
-  window.addEventListener("scroll", function () {
-    if (window.scrollY > divSottostante.offsetTop - header.offsetHeight) {
+  function handleScroll() {
+    if (window.scrollY > underHeader.offsetTop - header.offsetHeight) {
       header.classList.add("sticky");
+      navBtn.classList.add("sticky");
     } else {
       header.classList.remove("sticky");
+      navBtn.classList.remove("sticky");
     }
-  });
+  }
+  //funzione per transizione header bg-color
+  const header = document.querySelector("header");
+  const underHeader = document.querySelector(".div-trending");
+  const navBtn = document.querySelector(".nav-btn");
+  window.addEventListener("scroll", handleScroll);
 
   // funzione per SVG
-  let svgTop = document.querySelector(".SVG > svg");
-  let nodeOfM = svgTop.querySelectorAll("g[stroke-linecap]");
 
-  const intervalTop = setInterval(changeM, 10);
-
-  function changeM() {
-    let casual = nodeOfM[Math.floor(Math.random() * nodeOfM.length)];
-    casual.setAttribute("opacity", Math.floor(Math.random() * 2));
+  function animateSvg() {
+    let casualElement = svgElements[Math.floor(Math.random() * svgElements.length)];
+    casualElement.setAttribute("opacity", Math.floor(Math.random() * 2));
   }
+  const svgElements = document.querySelectorAll(".SVG svg g[stroke-linecap]");
+
+  setInterval(animateSvg, 10);
 };
